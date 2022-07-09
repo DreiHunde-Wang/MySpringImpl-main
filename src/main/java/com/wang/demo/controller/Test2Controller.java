ff8c30f02a2fdf5305490067a294cb69861ffd56
I569683 <haolin.wang@sap.com>
2022-07-09 16:06:04 +0800

    add

---
+++ src/main/java/com/wang/demo/controller/Test2Controller.java
@@ -0,0 +1,21 @@
+package com.wang.demo.controller;
+
+import com.wang.demo.model.ResponseEntity;
+import com.wang.demo.model.User;
+import com.wang.demo.service.UserService;
+import com.wang.spring.annotation.ioc.Autowired;
+import com.wang.spring.annotation.mvc.*;
+
+@Controller
+@RequestMapping("/test")
+public class Test2Controller {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/getUser")
+    @ResponseBody
+    public ResponseEntity getUser() throws Exception {
+        User user = userService.findUser(1);
+        return ResponseEntity.success(user);
+    }
+}
